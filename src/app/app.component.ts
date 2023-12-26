1d70a07fa482fbbc4e7775db875a56e76d90c678
arseniit <arseniit@gmail.com>
2023-12-26 18:41:18 +0100

    Add general structure. Add bootstrap styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'wcs-angular14-reactive-form-2';
+export class AppComponent implements OnInit{
+  users: User[] = [];
+
+  ngOnInit(): void {
+    this.users.push(new User(
+      'Luna Nightshade',
+      'luna.night@example.com',
+      'moonlight123',
+      '456 Celestial Blvd, Star City'
+    ));
+  }
 }
