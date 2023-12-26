7a85020c79db1bb878c2b2acd2191f235920546a
arseniit <arseniit@gmail.com>
2023-12-26 19:41:55 +0100

    Add ReactiveFormsModule and FormControl realization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './user.model';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,33 @@ import { User } from './user.model';
 export class AppComponent implements OnInit{
   users: User[] = [];
 
+  userName = new FormControl('');
+  userEmail = new FormControl('');
+  userPassword = new FormControl('');
+  userAddress = new FormControl('');
+
   ngOnInit(): void {
     this.users.push(new User(
       'Luna Nightshade',
       'luna.night@example.com',
       'moonlight123',
       '456 Celestial Blvd, Star City'
+    ));
+  }
+
+  onSubmit() {
+    const name =this.userName.value;
+    const mail = this.userEmail.value;
+    const password = this.userPassword.value;
+    const address = this.userAddress.value;
+
+    if(!name || !mail || !password || !address) return;
+
+    this.users.unshift(new User(
+      name,
+      mail,
+      password,
+      address,
     ));
   }
 }
